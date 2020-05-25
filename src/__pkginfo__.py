af9571b9800f2736d477f138998c7fd974a96896
PELLEGRINI <ericpellegrini76@gmail.com>
2020-05-25 16:10:01 +0200

    updated CHANGELOG and __pkginfo__.py files

--- src/__pkginfo__.py
+++ src/__pkginfo__.py
@@ -4,25 +4,21 @@
 #
 # **************************************************************************
 
-__description__  = "Analysis of water residence time based on gromacs MD trajectories"
+__description__ = "Analysis of water residence time based on gromacs MD trajectories"
 
 __long_description__ = """waterstay is a program designed for computing the residence time of water molecules around a given atom.
 """
 
-__author__           = "E.C. Pellegrini"
+__author__ = "E.C. Pellegrini"
 
-__author_email__     = "ericpellegrini76@gmail.com"
+__author_email__ = "ericpellegrini76@gmail.com"
 
-__maintainer__       = "E.C. Pellegrini"
+__maintainer__ = "E.C. Pellegrini"
 
 __maintainer_email__ = "ericpellegrini76@gmail.com"
 
-__repo__             = "https://gitlab.com/eurydice38/waterstay.git"
+__repo__ = "https://gitlab.com/eurydice38/waterstay.git"
 
-__license__          = "GPL 3"
+__license__ = "GPL 3"
 
-__version__          = "0.0.0"
-
-
-
-
+__version__ = "0.0.1"
